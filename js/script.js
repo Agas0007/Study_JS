65b3548581d628b9ecc723a4b94702afb09dbf3c
Andrey Babaryka <BabarykaSES@yandex.ru>
2019-10-17 13:44:46 +0300

    Первый комит

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let money;
+let income;
+let addExspenses;
+let deposit;
+let mission;
+let period;
+ 
+alert('Сообщение для вас: Удачи по жизни!');
+
+console.log('Сообщение в консоле, вывелось.')
